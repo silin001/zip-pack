25fbb01eea593ca094ffb525758987935820dba9
silin001 <862956812@qq.com>
2023-09-22 10:31:13 +0800

    fix: 主分支代码整理

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'; // commonjs模块转换插件
 import { nodeResolve } from '@rollup/plugin-node-resolve'; // 依赖引用插件
-// import typescript from '@rollup/plugin-typescript';  // 让 rollup 认识 ts 的代码
-// import nodePolyfills from 'rollup-plugin-node-polyfills'; // 模拟这些变量和 API，以便于在浏览器环境中使用。
-// 转义es5
-// import babel from "rollup-plugin-babel"; // babel 插件
-// import { eslint } from 'rollup-plugin-eslint'; // eslint插件
 import pkg from './package.json'
-// 一段自定义的内容，以下内容会添加到打包结果中
-// const footer = `if(typeof window !== 'undefined') {
-//   window._Dry_VERSION_ = '${pkg.version}'
-// }`
-
 export default {
   input: './index.js', // 打包的入口文件
   output: [
@@ -34,24 +24,9 @@ export default {
   ],
   plugins: [
     nodeResolve(),
-    // resolve({
-    //   browser: true
-    // }),
     commonjs(),
-    // typescript()
-    // babel({
-    //   exclude: 'node_modules/**',
-    //   runtimeHelpers: true,
-    //   // presets: ['@babel/preset-env']
-    // }),
-    // eslint({
-    //  throwOnError: true,
-    //  include: ['src/**'],
-    //  exclude: ['node_modules/**']
-    // })
   ],
   ignore: [
     "node_modules/**" // 忽略目录
   ]
-
 }
