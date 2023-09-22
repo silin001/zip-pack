bc1461834c9f72b8d44e342a58d7b565dd6ad305
silin001 <862956812@qq.com>
2023-09-22 15:14:14 +0800

    fix: 注释内容

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 // 入口文件向外暴露需要的成员
 const { dirToZipFun } = require('./src/zip')
-// const { sucess } = require('./src/util/index')
-dirToZipFun({})
+const { sucess } = require('./src/util/index')
 const testaaa = 300
 // 返回插件钩子，在打包结束时打包为zip
-// function pluginPackZip (options = {}) {
-//   return {
-//     name: 'plugin-zip-pack',
-//     apply: 'build',
-//     closeBundle () {
-//       console.log(sucess('Vite build completed!'));
-//       dirToZipFun(options)
-//     }
-//   }
-// }
-// module.exports = {
-//   pluginPackZip,
-//   testaaa
-// }
+function pluginPackZip (options = {}) {
+  return {
+    name: 'plugin-zip-pack',
+    apply: 'build',
+    closeBundle () {
+      console.log(sucess('Vite build completed!'));
+      dirToZipFun(options)
+    }
+  }
+}
+module.exports = {
+  pluginPackZip,
+  testaaa
+}
